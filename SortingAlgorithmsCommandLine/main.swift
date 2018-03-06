6a90a9f5436f3aec064059f660ade4e3970ea52e
Pan Jason <jpan@lcs-mba3130.lcs.on.ca>
2018-03-06 11:22:04 -0500

    Finished working algorithm with outer & inner loop

--- SortingAlgorithmsCommandLine/main.swift
+++ SortingAlgorithmsCommandLine/main.swift
@@ -24,29 +24,54 @@ print("Press any key to generate the unsorted list.", terminator: "")
 let input = readLine()
 
 // Make an empty array / list
-var numbers : [Int] = []
+var cards : [Int] = []
 
 // Populate the array
-for _ in 1...10 {
-    numbers.append( random(between: 1, and: 50) )
-}
+cards.append(3)
+cards.append(9)
+cards.append(1)
+cards.append(22)
+cards.append(20)
+cards.append(17)
+
 
 // Show the user the contents of the array
 print("-----")
 print("The contents of the unsorted list:")
-for i in 0...numbers.count - 1 {
-    print("Index \(i), value: \(numbers[i])")
+for i in 0...cards.count - 1 {
+    print("Index \(i), value: \(cards[i])")
 }
 print("-----")
 
 // ----------- Write code to sort the array here -----------
 print("Now sorting the array...")
 
+//let temporaryValue2 : Int
+//temporaryValue2 = cards[1]
+//cards[1] = cards[1+1]
+//cards[1+1] = temporaryValue2
+//outer loop
+
+for _ in 1...cards.count - 1 {
+    //inner loop
+    //repeat this 4 times
+    for i in 0...cards.count - 1 - 1 {
+        //if the first number in greater than the next one
+        if cards[i] > cards[i+1] {
+            //then switch cards
+            let tempVal = cards[i]
+            cards[i] = cards[i+1]
+            cards[i+1] = tempVal
+        }
+    }
+}
+
+
 // ----------- Final part of the program, show the sorted list -----------
 print("-----")
 print("The contents of the sorted list:")
-for i in 0...numbers.count - 1 {
-    print("Index \(i), value: \(numbers[i])")
+for i in 0...cards.count - 1 {
+    print("Index \(i), value: \(cards[i])")
 }
 print("-----")
 
